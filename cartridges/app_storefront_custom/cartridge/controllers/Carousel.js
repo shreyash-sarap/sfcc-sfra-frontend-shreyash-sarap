d980acf424e5d5467ed3eca9e16c6a01929e0de4
shreyash-sarap <yashsarap@gmail.com>
2024-02-28 11:25:25 +0530

    Update Carousel.js

--- cartridges/app_storefront_custom/cartridge/controllers/Carousel.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Carousel.js
@@ -1,8 +1,9 @@
 "use strict";
 
 var server = require("server");
-
+// Created a show endpoint to render the carousel.isml template
 server.get("Show", function(req, res, next) {
+    // Included all the slick URLs to be used in slider.
     var carouselURLs = {
         slickbasicURL: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css",
         slickthemeURL: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css",
@@ -11,17 +12,17 @@ server.get("Show", function(req, res, next) {
     };
 
     var imageURLs = [];
-
+    // A function to retrieve images and store in variable .
     function getImageURLs(id) {
         var baseURL = `https://source.unsplash.com/random/200x200?city,night=${id}`;
 
         return baseURL + id;
     }
-
+    // Added a loop to get 8 random images
     for (let i = 1; i < 9; i++) {
         imageURLs.push(getImageURLs(i));
     }
-
+    // Renddered the carousel.isml file
     res.render("/carousel", {
         carouselURLs,
         imageURLs
