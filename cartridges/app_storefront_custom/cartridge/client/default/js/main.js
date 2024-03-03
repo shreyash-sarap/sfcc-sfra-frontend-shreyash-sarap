2930920d72fa09d1a2dade6a29d0b81867f45240
shreyash-sarap <yashsarap@gmail.com>
2024-03-04 02:55:06 +0530

    Updated

--- cartridges/app_storefront_custom/cartridge/client/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/client/default/js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
     processInclude(require("../../../../../app_storefront_base/cartridge/client/default/js/main"));
     processInclude(require("./components/test"));
-    // Included the slider.js file here.
-    processInclude(require("./components/slider"));
-    processInclude(require("../../../../slick-1.8.1/slick/slick"));
-    processInclude(require("../../../../slick-1.8.1/slick/slick.min"));
-    processInclude(require("../../../../slick-1.8.1/slick/jquery.min"));
 });
