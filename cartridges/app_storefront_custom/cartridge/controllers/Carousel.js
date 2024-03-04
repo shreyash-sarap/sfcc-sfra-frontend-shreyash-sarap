b205e4b89c2f87304c1c2d3ac28e229f160970fc
shreyash-sarap <yashsarap@gmail.com>
2024-03-04 19:56:27 +0530

    Final Update

--- cartridges/app_storefront_custom/cartridge/controllers/Carousel.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Carousel.js
@@ -1,16 +1,7 @@
 "use strict";
-
 var server = require("server");
-
 server.get("Show", function(req, res, next) {
-    var carouselURLs = {
-        jqueryslickURL: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js",
-        slickURL: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
-    };
-    res.render("/carousel", {
-        carouselURLs
-    });
+    res.render("/carousel");
     return next();
 });
-
 module.exports = server.exports();
